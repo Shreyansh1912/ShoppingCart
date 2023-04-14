1978757253d0dd7caa5013e0bbfe3d63985dd6f0
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-04-15 02:40:08 +0530

    updated

---
+++ build/precache-manifest.ac70ff8c81887d1969635fde8baa6021.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64be6a54f8193767b77a6e0e4e613c7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4388945a614b883aa5b",
+    "url": "/static/css/main.35e2fc94.chunk.css"
+  },
+  {
+    "revision": "32b650c25670705f154e",
+    "url": "/static/js/2.7124a6e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7124a6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4388945a614b883aa5b",
+    "url": "/static/js/main.be42bafa.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
